809f1dbb08e9241ec36d11bae6098becb91f1853
Vasilisa-fs <vasilisa.bychkova.fs@gmail.com>
2026-03-20 23:25:15 +0200

    Done

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,13 @@
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message || err.name,
+    });
+  }
+  const isProd = process.env.NODE_ENV === 'production';
+  res.status(500).json({
+    message: isProd ? 'Server Error' : err.message,
+  });
+};
---
+++ src/server.js
@@ -0,0 +1,28 @@
+import helmet from 'helmet';
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import { logger } from './middleware/logger.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import notesRouter from './routes/notesRoutes.js';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(logger);
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+app.use(notesRouter);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+await connectMongoDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
